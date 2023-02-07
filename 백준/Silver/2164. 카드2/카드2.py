da96f1563d3aa091e46d4f9470c0411049c9273e
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-02-07 15:08:36 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 51020 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,7 +1,14 @@
 from collections import deque
 
-n = deque(range(1, int(input())+1,))
-while len(n) != 1:
-    n.popleft()
-    n.rotate(-1)
-print(n[0])
+n = int(input())
+q = deque(range(1, n+1))
+
+if(n == 1):
+    print(n)
+else:
+    while(True): # 카드가 한 장 남을 때까지
+        q.popleft()
+        if(len(q) == 1):
+            break
+        q.rotate(-1)
+    print(q[0])
