5f46e2b97c5c87d5bc06da80bb308f071305c60d
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-04-06 17:02:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,15 +1,30 @@
-from itertools import combinations
-
 def isPrime(n):
     for i in range(2, n):
         if(n % i == 0):
             return False
     return True
         
+        
 def solution(nums):
     answer = 0
-    comb_arr = list(combinations(nums, 3))
+    n = len(nums)
+    r = 3
+    arr = [0] * r
+    comb_arr = [] # 결과 배열
     
+    def combination(level, idx): # nCr
+        if(level == r):
+            comb_arr.append(arr.copy())
+            return
+
+        for i in range(idx, n):
+            arr[level] = nums[i]
+            combination(level + 1, i + 1)
+    
+    # 조합 생성
+    combination(0, 0)
+    
+    # 소수 판별
     for arr in comb_arr:
         if(isPrime(sum(arr))):
             answer += 1
