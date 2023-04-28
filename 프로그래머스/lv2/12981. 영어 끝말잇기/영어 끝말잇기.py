4e5aee227f8f5dcc826480dd53b6873fb61ef0b4
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-04-28 10:22:32 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,18 +1,15 @@
 def solution(n, words):
     answer = [0, 0]
-    word_list = set()
-    word_list.add(words[0])
+    user_word = set([words[0]])
     
     for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0] or words[i] in word_list:
+        if (words[i-1][-1] != words[i][0]) or (words[i] in user_word):
             if (i+1) % n == 0:
-                answer[0] = n
-                answer[1] = (i+1) // n
+                answer[0], answer[1] = n, (i+1) // n
             else:
-                answer[0] = (i+1) % n
-                answer[1] = (i+1) // n + 1
+                answer[0],answer[1] = (i+1) % n, (i+1) // n + 1
             break
         else:
-            word_list.add(words[i])
+            user_word.add(words[i])
 
     return answer
