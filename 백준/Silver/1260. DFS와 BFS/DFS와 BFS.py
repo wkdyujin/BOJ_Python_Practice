754913ca33ba69d34ca08c6f7ddbb680662a2020
Yujin <69359774+wkdyujin@users.noreply.github.com>
2024-01-20 15:05:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 492 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,35 +1,37 @@
 from collections import deque
 
-visited_dfs = []
-visited_bfs = []
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    n1, n2 = map(int, input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
-def dfs(v):
-    visited_dfs.append(v)
-    for i in graph[v]:
-        if(i not in visited_dfs):
-            dfs(i)       
-    return visited_dfs
+for nodes in graph:
+    nodes.sort()
 
-def bfs(v):
-    q = deque([v])
-    while q:
-        node = q.popleft()
-        if(node not in visited_bfs):
-            visited_bfs.append(node)
-            q.extend(graph[node])
-    return visited_bfs
+def dfs(V):
+    visited = set()
+    stack = [V]
+    while stack:
+        cur = stack.pop()
+        if cur not in visited:
+            print(cur, end = ' ')
+            visited.add(cur)
+            for i in range(len(graph[cur])-1, -1, -1):
+                stack.append(graph[cur][i])
 
+def bfs(V):
+    visited = set()
+    queue = deque([V])
+    while(queue):
+        cur = queue.popleft()
+        if cur not in visited:
+            print(cur, end = ' ')
+            visited.add(cur)
+            queue.extend(graph[cur])
+        
 
-n, m, v = map(int, input().split())
-graph = [[] for i in range(n + 1)]
-
-for _ in range(m):
-    num1, num2 = map(int, input().split())
-    graph[num1].append(num2)
-    graph[num2].append(num1)
-    
-for i in graph:
-    i.sort()
-    
-print(*dfs(v))
-print(*bfs(v))
+dfs(V)
+print()
+bfs(V)
